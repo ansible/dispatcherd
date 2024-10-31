3d436b395f5da46bfabdf96029ecf64e02eee557
Alan Rominger <arominge@redhat.com>
2024-10-30 21:34:45 -0400

    Broke schedules in recent commit, fix that (#16)

--- dispatcher/producers/scheduled.py
+++ dispatcher/producers/scheduled.py
@@ -9,21 +9,20 @@ class ScheduledProducer:
         self.task_schedule = task_schedule
         self.scheduled_tasks = []
 
-    async def start_producing(self, pool):
+    async def start_producing(self, dispatcher):
         for task_name, options in self.task_schedule.items():
             per_seconds = options['schedule'].total_seconds()
-            self.scheduled_tasks.append(asyncio.create_task(self.run_schedule_forever(task_name, per_seconds, pool)))
+            self.scheduled_tasks.append(asyncio.create_task(self.run_schedule_forever(task_name, per_seconds, dispatcher)))
 
     def all_tasks(self):
         return self.scheduled_tasks
 
-    async def run_schedule_forever(self, task_name, per_seconds, pool):
+    async def run_schedule_forever(self, task_name, per_seconds, dispatcher):
         logger.info(f"Starting task runner for {task_name} with interval {per_seconds} seconds")
         while True:
             await asyncio.sleep(per_seconds)
             logger.info(f"Sending scheduled task to worker: {task_name}")
-            # TODO: this will be JSON data with more supporting stuff
-            await pool.dispatch_task({"task": task_name})
+            await dispatcher.process_message(task_name)
 
     async def shutdown(self):
         logger.info('Stopping scheduled tasks')
