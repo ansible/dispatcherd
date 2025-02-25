ce5ef6fc1c5cd2d50c611ea5f4bdabea5beac329
Alan Rominger <arominge@redhat.com>
2025-02-25 06:49:07 -0500

    Basics of an on-start producer

--- dispatcher/producers/__init__.py
+++ dispatcher/producers/__init__.py
@@ -1,5 +1,6 @@
 from .base import BaseProducer
 from .brokered import BrokeredProducer
 from .scheduled import ScheduledProducer
+from .on_start import OnStartProducer
 
-__all__ = ['BaseProducer', 'BrokeredProducer', 'ScheduledProducer']
+__all__ = ['BaseProducer', 'BrokeredProducer', 'ScheduledProducer', 'OnStartProducer']
