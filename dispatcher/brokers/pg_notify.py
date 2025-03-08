23230efff1a42b889b3be72e9fd1f739d85a5715
Alan Rominger <arominge@redhat.com>
2025-03-08 08:26:23 -0500

    Add socket broker
    Protocol the brokers
    
    Run demo dispatcherctl over socket
    
    Add socket broker unit tests
    Add socket broker usage integration tests
    
    Work out issues with test scope and server not opening client connections

--- dispatcher/brokers/pg_notify.py
+++ dispatcher/brokers/pg_notify.py
@@ -6,6 +6,8 @@ import psycopg
 
 from dispatcher.utils import resolve_callable
 
+from ..protocols import Broker as BrokerProtocol
+
 logger = logging.getLogger(__name__)
 
 
@@ -32,7 +34,7 @@ def create_connection(**config) -> psycopg.Connection:  # type: ignore[no-untype
     return connection
 
 
-class Broker:
+class Broker(BrokerProtocol):
     NOTIFY_QUERY_TEMPLATE = 'SELECT pg_notify(%s, %s);'
 
     def __init__(
@@ -157,7 +159,7 @@ class Broker:
         """The inner logic of async message publishing where we already have a cursor"""
         await cursor.execute(self.NOTIFY_QUERY_TEMPLATE, (channel, message))
 
-    async def apublish_message(self, channel: Optional[str] = None, message: str = '') -> None:  # public
+    async def apublish_message(self, channel: Optional[str] = None, origin: Union[str, int, None] = '', message: str = '') -> None:  # public
         """asyncio way to publish a message, used to send control in control-and-reply
 
         Not strictly necessary for the service itself if it sends replies in the workers,
