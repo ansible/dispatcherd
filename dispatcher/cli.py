c559c6e90d73e3c2e74b65bbec79dcf1c1c53ea4
Alan Rominger <arominge@redhat.com>
2025-03-07 20:26:17 -0500

    Start on socket broker
    Add dispatcherctl entrypoint and refactor demo
    
    Start on linters for socket
    
    Move socket to another branch
    
    Revert change not needed
    
    Fully support communication with 2 services
    
    Install not needed for docker
    
    Target to bring down demo for completeness

--- dispatcher/cli.py
+++ dispatcher/cli.py
@@ -3,13 +3,16 @@ import logging
 import os
 import sys
 
+import yaml
+
 from . import run_service
 from .config import setup
+from .factories import get_control_from_settings
 
 logger = logging.getLogger(__name__)
 
 
-def standalone() -> None:
+def get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="CLI entrypoint for dispatcher, mainly intended for testing.")
     parser.add_argument(
         '--log-level',
@@ -24,12 +27,51 @@ def standalone() -> None:
         default='dispatcher.yml',
         help='Path to dispatcher config.',
     )
+    return parser
 
+
+def setup_from_parser(parser) -> argparse.Namespace:
     args = parser.parse_args()
     logging.basicConfig(level=getattr(logging, args.log_level), stream=sys.stdout)
 
     logger.debug(f"Configured standard out logging at {args.log_level} level")
 
     setup(file_path=args.config)
+    return args
 
+
+def standalone() -> None:
+    setup_from_parser(get_parser())
     run_service()
+
+
+def control() -> None:
+    parser = get_parser()
+    parser.add_argument('command', help='The control action to run.')
+    parser.add_argument(
+        '--task',
+        type=str,
+        default=None,
+        help='Task name to filter on.',
+    )
+    parser.add_argument(
+        '--uuid',
+        type=str,
+        default=None,
+        help='Task uuid to filter on.',
+    )
+    parser.add_argument(
+        '--expected-replies',
+        type=int,
+        default=1,
+        help='Expected number of replies, in case you are have more than 1 service running.',
+    )
+    args = setup_from_parser(parser)
+    data = {}
+    for field in ('task', 'uuid'):
+        val = getattr(args, field)
+        if val:
+            data[field] = val
+    ctl = get_control_from_settings()
+    returned = ctl.control_with_reply(args.command, data=data, expected_replies=args.expected_replies)
+    print(yaml.dump(returned, default_flow_style=False))
--- run_demo.py
+++ run_demo.py
@@ -25,6 +25,15 @@ TEST_MSGS = [
 ]
 
 
+# If we run against more than 1 background task service,
+# we need to wait for that-many replies, so allow this via sys.argv last arg
+expected_count = 1
+try:
+    expected_count = int(sys.argv[-1])
+except ValueError:
+    pass
+
+
 def main():
     print('writing some basic test messages')
     for channel, message in TEST_MSGS:
@@ -44,18 +53,18 @@ def main():
     print('performing a task cancel')
     # we will "find" a task two different ways here
     broker.publish_message(message=json.dumps({'task': 'lambda: __import__("time").sleep(3.1415)', 'uuid': 'foobar'}))
-    canceled_jobs = ctl.control_with_reply('cancel', data={'uuid': 'foobar'})
+    canceled_jobs = ctl.control_with_reply('cancel', data={'uuid': 'foobar'}, expected_replies=expected_count)
     print(json.dumps(canceled_jobs, indent=2))
 
     print('')
     print('finding a running task by its task name')
     broker.publish_message(message=json.dumps({'task': 'lambda: __import__("time").sleep(3.1415)', 'uuid': 'find_by_name'}))
-    running_data = ctl.control_with_reply('running', data={'task': 'lambda: __import__("time").sleep(3.1415)'})
+    running_data = ctl.control_with_reply('running', data={'task': 'lambda: __import__("time").sleep(3.1415)'}, expected_replies=expected_count)
     print(json.dumps(running_data, indent=2))
 
     print('')
     print('getting worker status')
-    worker_data = ctl.control_with_reply('workers')
+    worker_data = ctl.control_with_reply('workers', expected_replies=expected_count)
     print(json.dumps(worker_data, indent=2))
 
     print('')
@@ -72,7 +81,7 @@ def main():
 
     print('')
     print('run bogus control command')
-    worker_data = ctl.control_with_reply('not-a-command')
+    worker_data = ctl.control_with_reply('not-a-command', expected_replies=expected_count)
     print(json.dumps(worker_data, indent=2))
 
     print('writing a message with a delay')
@@ -94,20 +103,20 @@ def main():
 
     print('')
     print('showing delayed tasks in running list')
-    running_data = ctl.control_with_reply('running', data={'task': f'tests.data.methods{MODULE_METHOD_DELIMITER}sleep_function'})
+    running_data = ctl.control_with_reply('running', data={'task': f'tests.data.methods{MODULE_METHOD_DELIMITER}sleep_function'}, expected_replies=expected_count)
     print(json.dumps(running_data, indent=2))
 
     print('')
     print('cancel a delayed task with no reply for demonstration')
     ctl.control('cancel', data={'task': f'tests.data.methods{MODULE_METHOD_DELIMITER}sleep_function'})  # NOTE: no reply
     print('confirmation that it has been canceled')
-    running_data = ctl.control_with_reply('running', data={'task': f'tests.data.methods{MODULE_METHOD_DELIMITER}sleep_function'})
+    running_data = ctl.control_with_reply('running', data={'task': f'tests.data.methods{MODULE_METHOD_DELIMITER}sleep_function'}, expected_replies=expected_count)
     print(json.dumps(running_data, indent=2))
 
     print('')
     print('running alive check a few times')
     for i in range(3):
-        alive = ctl.control_with_reply('alive')
+        alive = ctl.control_with_reply('alive', expected_replies=expected_count)
         print(alive)
 
     print('')
