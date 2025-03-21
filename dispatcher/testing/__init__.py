ef24641731e281105e8a3724febc79d0c5e8ad69
Alan Rominger <arominge@redhat.com>
2025-03-21 15:23:51 -0400

    Run isort

--- dispatcher/testing/__init__.py
+++ dispatcher/testing/__init__.py
@@ -1,4 +1,4 @@
-from .subprocess import dispatcher_service
 from .asyncio import adispatcher_service
+from .subprocess import dispatcher_service
 
 __all__ = ['adispatcher_service', 'dispatcher_service']
