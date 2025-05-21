f01344e5ce0cff0ab8782feb0a72e4d45cf2de06
Alan Rominger <arominge@redhat.com>
2025-05-21 11:33:01 -0400

    Fix infinite hang

--- tests/test_noop_broker.py
+++ tests/test_noop_broker.py
@@ -4,6 +4,16 @@ import pytest
 from dispatcherd.brokers.noop import Broker
 
 
+class StreamYields:
+    def __init__(self):
+        self.received = []
+
+    async def run(self):
+        broker = Broker()
+        async for channel, message in broker.aprocess_notify():
+            self.received.append((channel, message))
+
+
 @pytest.mark.asyncio
 async def test_noop_broker_apublish_message():
     """Test that apublish_message does nothing."""
@@ -15,11 +25,15 @@ async def test_noop_broker_apublish_message():
 @pytest.mark.asyncio
 async def test_noop_broker_aprocess_notify():
     """Test that aprocess_notify yields empty messages."""
-    broker = Broker()
-    async for channel, message in broker.aprocess_notify():
-        assert channel == ''
-        assert message == ''
-        break  # Just test one iteration
+    streamer = StreamYields()
+    str_task = asyncio.create_task(streamer.run())
+    await asyncio.sleep(0.01)
+    str_task.cancel()
+    try:
+        await str_task
+    except asyncio.CancelledError:
+        pass
+    assert len(streamer.received) == 0  # assert we got no messages
 
 
 def test_noop_broker_publish_message():
