ea7bf3563c35dafdc9e8ef0867f51653656a5d59
AlanCoding <arominge@redhat.com>
2025-12-19 08:55:21 -0500

    Run linters, update schema

--- tests/testing/test_debug_brokers.py
+++ tests/testing/test_debug_brokers.py
@@ -55,18 +55,14 @@ async def test_aprocess_notify_is_cancelable(broker):
 def test_memory_broker_records_sync_messages():
     broker = MemoryBroker()
     broker.publish_message(channel='chan', message='payload')
-    assert broker.published_messages == [
-        {'channel': 'chan', 'origin': None, 'message': 'payload', 'is_async': False}
-    ]
+    assert broker.published_messages == [{'channel': 'chan', 'origin': None, 'message': 'payload', 'is_async': False}]
 
 
 @pytest.mark.asyncio
 async def test_memory_broker_records_async_messages():
     broker = MemoryBroker()
     await broker.apublish_message(channel='chan', origin='origin', message='payload')
-    assert broker.published_messages == [
-        {'channel': 'chan', 'origin': 'origin', 'message': 'payload', 'is_async': True}
-    ]
+    assert broker.published_messages == [{'channel': 'chan', 'origin': 'origin', 'message': 'payload', 'is_async': True}]
 
 
 def test_memory_broker_process_notify_respects_max_messages():
@@ -95,4 +91,3 @@ async def test_memory_broker_aprocess_notify_yields_enqueued_notifications():
 def test_get_broker_accepts_dotted_import_path():
     broker = get_broker('dispatcherd.testing.brokers.memory', {})
     assert isinstance(broker, MemoryBroker)
-
